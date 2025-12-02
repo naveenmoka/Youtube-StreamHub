7a5adb8231e681a143b2f9cf89d36dc5541d7e46
naveenmoka <naveenmoka2004@gmail.com>
2025-12-02 12:13:23 +0530

    "Fix mixed content error: updated API to https" and create env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY = "AIzaSyB_7LgiRfU-Ix-mK-USwVA1U8q-0F56vA8";
+export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const USER_ICON =
   "https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png";
@@ -7,6 +7,6 @@ export const YOUTUBE_VIDEO_API =
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const LIVE_CHAT_COUNT = 25;
